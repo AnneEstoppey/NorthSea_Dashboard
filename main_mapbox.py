7107677ee672e0a64c40d12b14f0b1f414a0b134
Anne Estoppey <anne.estoppey@gmail.com>
2023-12-30 20:05:51 +0100

    main updates

--- main_mapbox.py
+++ main_mapbox.py
@@ -9,7 +9,7 @@ import numpy as np
 # Your Mapbox Access Token
 ## mapbox_access_token = MAPBOX_TOKEN
 
-mapbox_access_token = st.secrets["MABOX_TOKEN"]
+mapbox_access_token = st.secrets["MAPBOX_TOKEN"]
 
 # Page configuration
 st.set_page_config(
