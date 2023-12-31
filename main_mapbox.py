b312169ef6dd0898cb183235533b9c1f271364ca
Anne Estoppey <anne.estoppey@gmail.com>
2023-12-31 16:41:25 +0100

    main updates

--- main_mapbox.py
+++ main_mapbox.py
@@ -41,7 +41,7 @@ st.set_page_config(
 st.markdown("""
     <style>
             .block-container {
-                padding-top: 1rem;
+                padding-top: 0.5rem;
                 padding-bottom: 0rem;
                 padding-left: 1rem;
                 padding-right: 1rem;
